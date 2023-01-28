715575fa13c0a175cc1ead6a58c878e85a07ced0
artem-xopc <romanovartem.2000@mail.ru>
2023-01-28 21:06:44 +0300

    reducer refactoring

--- src/redux/about_reducer.js
+++ src/redux/about_reducer.js
@@ -3,31 +3,35 @@ const UPDATE_POST = "UPDATE-POST";
 const ADD_COMMENT = "ADD-COMMENT";
 const UPDATE_COMMENT = "UPDATE-COMMENT";
 
-
 const aboutReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {
-          id: 4,
-          tittle: state.newTittleText,
-          post: state.newPostText,
-          likes: 0,
-        };
-  
-        state.posts.push(newPost);
-        state.newTittleText = "";
-        state.newPostText = "";
-      } else if (action.type === UPDATE_POST) {
-        state.newPostText = action.newText;
-        // state.newTittleText = newText;
-      } else if (action.type === ADD_COMMENT) {
-        let messageBody = state.about.newCommentBody;
-        state.newCommentBody = "";
-        state.comments.push({ id: 4, body: messageBody });
-      } else if (action.type === UPDATE_COMMENT) {
-        state.newCommentBody = action.body;
-      }
-  
-    return state;
-}
+  switch (action.type) {
+    case ADD_POST:
+      let newPost = {
+        id: 4,
+        tittle: state.newTittleText,
+        post: state.newPostText,
+        likes: 0,
+      };
+
+      state.posts.push(newPost);
+      state.newTittleText = "";
+      state.newPostText = "";
+      return state;
+    case UPDATE_POST:
+      state.newPostText = action.newText;
+      // state.newTittleText = newText;
+      return state;
+    case ADD_COMMENT:
+      let messageBody = state.about.newCommentBody;
+      state.newCommentBody = "";
+      state.comments.push({ id: 4, body: messageBody });
+      return state;
+    case UPDATE_COMMENT:
+      state.newCommentBody = action.body;
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default aboutReducer;
